4911e748ca72d00e2c3745b17061654ccf26f969
Aman Ahamed <amanahamed04gmail.com>
2025-01-27 18:16:44 +0530

    data binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent {
+  courseName: string = 'Angular 19';
+  courseDur = '3 hrs';
+  myPlaceHolder = 'Enter your name';
+  clsName = 'bg-success';
+  isActive = true;
 
+  shiwWelcomeText() {
+    alert('holaa');
+  }
+
+  showMsgmsg(msg: string) {
+    alert(msg);
+  }
+
+  onStateChange() {
+    alert('changed');
+  }
 }
